a3b165e5d4b47a12bd69c938bc2babc41603e134
Zhaoqi Zhu <zhaoqizhu96@gmail.com>
2022-10-10 10:57:43 -0700

    Update wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -1,3 +1,15 @@
+# Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file
+# except in compliance with the License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS"
+# BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied. See the License for
+# the specific language governing permissions and limitations under the License.
+
+
 num_efa_devices=`lstopo | grep OpenFabrics | cut -d'"' -f2 | wc -l`
 efa_device_idx=1
 if [ "$num_efa_devices" -eq "0" ]; then # Mostly will be p3.16 block
